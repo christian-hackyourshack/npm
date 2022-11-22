1f63eb1296e7c21866d9223d875401fb6dbdac9f
Christian Fuss <christian@hackyourshack.org>
2022-11-22 10:26:01 +0000

    Finalized the blog on images

--- apps/astro-m2dx/src/content/_directives.ts
+++ apps/astro-m2dx/src/content/_directives.ts
@@ -2,10 +2,12 @@ import { BlogList } from '@components/BlogList';
 import { Card } from '@components/Card';
 import { CTA } from '@components/CTA';
 import { Quote } from '@components/Quote';
+import { Signature } from '@components/Signature';
 
 export const directives = {
   BlogList,
   Card,
   CTA,
   Quote,
+  Signature,
 };
