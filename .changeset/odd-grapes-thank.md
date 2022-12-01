e77fa27288574bddf1aaaf37f82eee24dd5f5518
Christian Fuss <christian@hackyourshack.org>
2022-12-01 08:04:24 +0000

    Upgraded dependencies

---
+++ .changeset/odd-grapes-thank.md
@@ -0,0 +1,8 @@
+---
+'astro-m2dx': patch
+'astro-m2dx-image': patch
+'m2dx-utils': patch
+'mintest-green': patch
+---
+
+Upgraded dependencies
--- packages/astro-m2dx-image/lib/types.ts
+++ packages/astro-m2dx-image/lib/types.ts
@@ -2,9 +2,17 @@ import type { ImgHTMLAttributes } from '@astrojs/image/components';
 import type { OutputFormat, TransformOptions } from '@astrojs/image/dist/loaders';
 import type { ImageMetadata } from '@astrojs/image/dist/vite-plugin-astro-image';
 
-type AspectRatio = TransformOptions['aspectRatio'];
+export type { ImageMetadata };
 
-interface ImageProps
+export type AspectRatio = TransformOptions['aspectRatio'];
+export type Background = TransformOptions['background'];
+export type Decoding = HTMLImageElement['decoding'];
+export type Fit = TransformOptions['fit'];
+export type Format = TransformOptions['format'];
+export type Loading = HTMLImageElement['loading'];
+export type Position = TransformOptions['position'];
+
+export interface ImageProps
   extends Omit<TransformOptions, 'src'>,
     Omit<ImgHTMLAttributes, 'slot' | 'alt' | 'src' | 'width' | 'height'> {
   /** Defines an alternative text description of the image. Set to an empty string (alt="") if the image is not a key part of the content (it's decoration or a tracking pixel). */
@@ -12,24 +20,20 @@ interface ImageProps
   src: string | ImageMetadata | Promise<{ default: ImageMetadata }>;
 }
 
-interface BasePictureProps extends Omit<ImageProps, 'format'> {
+/**
+ * The Picture environment tries to resolve the src and calls a user callback,
+ * allowing to transform the props, before optimizing the image according to
+ * the transformed properties.
+ */
+export interface PictureProps extends Omit<ImageProps, 'format'> {
   widths?: number[];
   formats?: OutputFormat[];
-}
-
-interface CallbackProps extends Omit<BasePictureProps, 'src'> {
-  src: ImageMetadata;
-}
-
-interface PictureProps extends BasePictureProps {
   callback?: (props: CallbackProps) => void | Promise<void>;
 }
 
-export type {
-  //
-  AspectRatio,
-  CallbackProps,
-  ImageMetadata,
-  ImageProps,
-  PictureProps,
-};
+/**
+ * We guarantee, that src is resolved before any callback is called.
+ */
+export interface CallbackProps extends Omit<PictureProps, 'src' | 'callback'> {
+  src: ImageMetadata;
+}
